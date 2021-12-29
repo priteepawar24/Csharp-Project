3f23e711bffc97110510451752dbd57627880129
priteepawar24 <priteepawar21815@gmail.com>
2021-12-29 16:03:01 +0530

    added pritee.txt

---
+++ ConsoleApp44/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp44
+{
+    class Program
+    {
+        public static void reverseWords(string str)
+        {
+            Stack<char> st = new Stack<char>();
+
+
+            // Traverse given string and push 
+
+            // all characters to stack until
+
+            // we see a space. 
+
+            for (int i = 0; i < str.Length; ++i)
+
+            {
+
+                if (str[i] != ' ')
+
+                {
+
+                    st.Push(str[i]);
+
+                }
+
+
+                // When we see a space, we 
+
+                // print contents of stack. 
+
+                else
+
+                {
+
+                    while (st.Count > 0)
+
+                    {
+
+                        Console.Write(st.Pop());
+
+
+                    }
+
+                    Console.Write(" ");
+
+                }
+
+            }
+
+
+            // Since there may not be 
+
+            // space after last word. 
+
+            while (st.Count > 0)
+
+            {
+
+                Console.Write(st.Pop());
+
+
+            }
+        }
+
+        // Driver Code
+
+        public static void Main(string[] args)
+        {
+
+            string str = "Welcome to Csharp corner";
+
+            reverseWords(str);
+            Console.ReadLine();
+
+        }
+    }
+}
